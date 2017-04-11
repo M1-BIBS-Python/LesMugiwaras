551c82fc05e89dfb533f472d71c0c8b81d93816e
Peloye <hugo.arbes@u-psud.fr>
2017-04-11 21:56:27 +0200

    Centre de masse et Rayon

--- structureTools_HugoArbes.py
+++ structureTools_HugoArbes.py
@@ -50,56 +50,91 @@ def ParserPDB(a):
 			acidea[newProt][chaine][residu][atome]["z"] = contenu[chain][48:54] #Meme chose pour Z
 			acidea[newProt][chaine][residu][atome]["Id"] = contenu[chain][9:11] #Meme chose pour Identifiant
 
-	return( acidea)
+	return(acidea)
 
 
-def CentreMasseResidu(dictionnaire_residu):	# Mettre dico de 1 residu
+def CentreMasseProt(dictionnaire_proteine):
+	
+	nbr_residus = 0
+	x = 0
+	y = 0
+	z = 0
+	centre_masse_prot = {}
+	
+	# Ajout de toutes les coordonnees des centres de masse de tous les residus
+	for Proteine in dictionnaire_proteine.keys():
+		for Chaine in dictionnaire_proteine[Proteine].keys():
+			for Residu in dictionnaire_proteine[Proteine][Chaine].keys():
+				x += CentreMasseResidu(dictionnaire_proteine[Proteine][Chaine][Residu])['x']
+				y += CentreMasseResidu(dictionnaire_proteine[Proteine][Chaine][Residu])['y']
+				z += CentreMasseResidu(dictionnaire_proteine[Proteine][Chaine][Residu])['z']
+				nbr_residus += 1
+				
+	centre_masse_prot['x'] = x / nbr_residus
+	centre_masse_prot['y'] = y / nbr_residus
+	centre_masse_prot['z'] = z / nbr_residus
+	
+	return centre_masse_prot
+
+def CentreMasseResidu(dictionnaire_residu):
 	
 	atomicMass = dict()
-	atomicMass = {"C": float(12.0107), "N": float(14.0067), 
-				  "O": float(15.9994), "S": float(32.065),
+	atomicMass = {"C": float(12.0107), "S": float(32.065), 
+				  "O ": float(15.9994), "N ": float(14.0067),
 				  "OH": float(17.0073), "NH": float(15.0146)}
 	
 	x = 0
 	y = 0
 	z = 0
-	mass_totale = 0
-	cm_res = {}
+	masse_totale_residu = 0
+	centre_masse_residu = {}
 	
 	# Ajout de toutes les coordonnees des atomes du residu
 	for atom_compare in dictionnaire_residu.keys():
 			for atom_reference in atomicMass.keys():
 				if atom_reference in atom_compare:
-					mass = atomicMass[atom_reference]
+					masse_atome = atomicMass[atom_reference]
 				
-			x += mass*float(dictionnaire_residu[atom_compare]['x'])
-			y += mass*float(dictionnaire_residu[atom_compare]['y'])
-			z += mass*float(dictionnaire_residu[atom_compare]['z'])
-			mass_totale += mass
-			#~ print(mass)
-	# On divise par la masse totale des atomes pour avoir les coordonnees du centre de masse du residu:
-	cm_res['x'] = x/mass_totale
-	cm_res['y'] = y/mass_totale
-	cm_res['z'] = z/mass_totale
+			x += masse_atome * float(dictionnaire_residu[atom_compare]['x'])
+			y += masse_atome * float(dictionnaire_residu[atom_compare]['y'])
+			z += masse_atome * float(dictionnaire_residu[atom_compare]['z'])
+			masse_totale_residu += masse_atome
+			
+	# On divise par la masse totale des atomes pour avoir les coordonnees du centre de masse du residu
+	centre_masse_residu['x'] = x / masse_totale_residu
+	centre_masse_residu['y'] = y / masse_totale_residu
+	centre_masse_residu['z'] = z / masse_totale_residu
 	
-	return cm_res
+	return centre_masse_residu
+
+
+
+def RayonProt(dictionnaire_proteine):
+	
+	CdM_prot = CentreMasseProt(dictionnaire_proteine)
+	le_plus_loin = 0
+	
+	for Proteine in dictionnaire_proteine.keys():
+		for Chaine in dictionnaire_proteine[Proteine].keys():
+			for Residu in dictionnaire_proteine[Proteine][Chaine].keys():
+				parcourt = CentreMasseResidu(dictionnaire_proteine[Proteine][Chaine][Residu])
+				distant = sqrt(pow(CdM_prot['x']-parcourt['x'],2) + pow(CdM_prot['y']-parcourt['y'],2) + pow(CdM_prot['z']-parcourt['z'],2))
+				if distant > le_plus_loin :
+					le_plus_loin = distant
+	
+	return le_plus_loin
 
 
 
 if __name__ == '__main__':
 	
 	import json
-	print("Donnees pour l'arginine : \n"+json.dumps(ParserPDB("1EJH.pdb"), indent = 4))
+	print(json.dumps(ParserPDB("1EJH.pdb"), indent = 4))
 	
 	#~ fichier = raw_input ("Saississez le nom de votre fichier avec le format (ex: arginine.pdb):")
 	
 	dico = ParserPDB("1EJH.pdb")
-	#~ print(dico)
-	#~ print("\n")
-	#~ print(dico.keys())
-	#~ print(dico["1"].keys())
-	#~ print(dico["1"]["A"].keys())
-	#~ print(dico["1"]["A"]["03"].keys())
-	CentreDeMasse = CentreMasseResidu(dico["1"]["A"]["03"])
-	print(CentreDeMasse)
-
+	CentreDeMasseProt = CentreMasseProt(dico)
+	print CentreDeMasseProt
+	rayon_proteine = RayonProt(dico)
+	print(rayon_proteine)

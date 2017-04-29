424a70e556c2ebf10d0c88aa8216ae0dff86962c
Adrien <adrien@localhost.localdomain>
2017-04-29 03:28:23 +0200

    Ajout du main realisant le projet

---
+++ BarStar.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+
+"""
+Auteur : HENRIQUES Adrien et ARBES Hugo 
+date : 28/04/2017
+Programme BarStar : Appel l'ensemble des fonctions crées pour realiser le projet BarStar.
+"""
+########### IMPORT ################"
+from Scribe import ScribeGlobal
+from Scribe import ScribeLocal
+from RMSDlocaux import RMSDlocaux
+from RMSDlocaux import graph
+from RMSDglobaux import RMSD
+from RMSDglobaux import graphglob
+from TestParcer import ParserPDB
+from TestParcer import Affichage
+
+
+########### MAIN ###############
+
+try:
+	fichier1 = raw_input ("Saississez le nom de votre fichier de reference avec le format (ex: arginine.pdb):")
+	result_fichier1 = ParserPDB(fichier1)
+	#Affichage(result_fichier1)
+	fichier2 = raw_input ("Saississez le nom de votre fichier avec le format (ex: arginine.pdb):")
+	result_fichier2 = ParserPDB(fichier2)
+	#Affichage(result_fichier1)
+		
+	#Appel des fonction de calcul de RMSD global + affichage
+	RmsdGlobal= RMSD(result_fichier1,result_fichier2 )
+	graphglob(RmsdGlobal)
+	ScribeGlobal(RmsdGlobal) #########################################AJOUTER LE DICTIONNAIRE VOIR SCRIBE
+	
+	#Appel des fonction de calcul de RMSD locale + affichage
+	RmsdLocal= RMSDlocaux(result_fichier1,result_fichier2 )
+	graph(RmsdLocal)
+	ScribeLocal(RmsdLocal) #########################################AJOUTER LE DICTIONNAIRE VOIR SCRIBE
+
+	
+except:
+	print("L'un des deux fichier specifié n'existe pas ! ")
+
+## Revoir la gestion d'erreur car elle m'affiche toujours ce message d'erreur meme si l'erreur survient apres l'entree des fichier 
